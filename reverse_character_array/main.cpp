75e8c398c777ab90767d00910a95e1b8c96dd456
Mike Williams <mumakin@gmail.com>
2019-10-02 09:05:07 +0200

    cheat using algorithm

--- reverse_character_array/main.cpp
+++ reverse_character_array/main.cpp
@@ -1,7 +1,38 @@
 #define CATCH_CONFIG_MAIN 
 #include "catch2/catch.hpp"
 
-TEST_CASE("Happy Path")
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+std::string ReverseString(const char* value)
 {
-   REQUIRE(false);
+   std::string result {value};
+   std::reverse(result.begin(), result.end());
+   return result;
+}
+
+TEST_CASE("Hello world")
+{
+   REQUIRE(0 == ReverseString("hello world").compare("dlrow olleh"));
+}
+
+TEST_CASE("Odd characters")
+{
+   REQUIRE(0 == ReverseString("abc").compare("cba"));
+}
+
+TEST_CASE("Even characters")
+{
+   REQUIRE(0 == ReverseString("abcd").compare("dcba"));
+}
+
+TEST_CASE("Single character")
+{
+   REQUIRE(0 == ReverseString("r").compare("r"));
+}
+
+TEST_CASE("Empty")
+{
+   REQUIRE(0 == ReverseString("").compare(""));
 }
